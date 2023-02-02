b3c6e5192a4e90e75fddbf410fc8f4ec358b8ca4
am <am2022a2@gmail.com>
2023-02-02 23:40:14 +0330

    put functions in data class

--- venv/src/Backend/PhoneBookBack.py
+++ venv/src/Backend/PhoneBookBack.py
@@ -2,72 +2,68 @@ import sqlite3
 
 con = sqlite3.connect(r'./PBDB.db')
 
-def LoadAll(con):
+class data:
+    def LoadAll(con):
 
-    cursorObj = con.cursor()
+        cursorObj = con.cursor()
 
-    cursorObj.execute('SELECT * FROM contact')
+        cursorObj.execute('SELECT * FROM contact')
 
-    rows = cursorObj.fetchall()
+        rows = cursorObj.fetchall()
 
-    return rows
+        return rows
 
-def Add(con,name,family,phonenumber):
-    entities = (name,family,phonenumber)
-    cursorObj = con.cursor()
-    cursorObj.execute("INSERT INTO contact(Name,Family,PhoneNumber)VALUES (?,?,?)",entities)
-    con.commit()
-
-
-
-def Remove(con,contactID):
-    cursorObj = con.cursor()
-    cursorObj.execute(f"DELETE FROM contact WHERE ID='{contactID}'")
-    con.commit()
-
-def Update(con,contactID,name,family,phonenumber):
-    cursorObj = con.cursor()
-    cursorObj.execute(f"UPDATE contact SET Name='{name}',Family='{family}',PhoneNumber='{phonenumber}' WHERE ID='{contactID}'")
-    con.commit()
-
-
-def SearchByID(con,contactID):
-    cursorObj = con.cursor()
-
-    cursorObj.execute(f"SELECT * FROM contact WHERE ID='{contactID}'")
-
-    rows = cursorObj.fetchall()
-
-    return rows
-
-def SearchByName(con,name):
-    cursorObj = con.cursor()
-
-    cursorObj.execute(f"SELECT * FROM contact WHERE Name='{name}'")
-
-    rows = cursorObj.fetchall()
-
-    return rows
-
-def SearchByFamily(con,family):
-    cursorObj = con.cursor()
-
-    cursorObj.execute(f"SELECT * FROM contact WHERE Family='{family}'")
-
-    rows = cursorObj.fetchall()
-
-    return rows
-
-def SearchByPhoneNumber(con,phonenumber):
-    cursorObj = con.cursor()
-
-    cursorObj.execute(f"SELECT * FROM contact WHERE PhoneNumber='{phonenumber}'")
-
-    rows = cursorObj.fetchall()
-
-    return rows
+    def Add(con,name,family,phonenumber):
+        entities = (name,family,phonenumber)
+        cursorObj = con.cursor()
+        cursorObj.execute("INSERT INTO contact(Name,Family,PhoneNumber)VALUES (?,?,?)",entities)
+        con.commit()
 
 
 
+    def Remove(con,contactID):
+        cursorObj = con.cursor()
+        cursorObj.execute(f"DELETE FROM contact WHERE ID='{contactID}'")
+        con.commit()
+
+    def Update(con,contactID,name,family,phonenumber):
+        cursorObj = con.cursor()
+        cursorObj.execute(f"UPDATE contact SET Name='{name}',Family='{family}',PhoneNumber='{phonenumber}' WHERE ID='{contactID}'")
+        con.commit()
 
 
+    def SearchByID(con,contactID):
+        cursorObj = con.cursor()
+
+        cursorObj.execute(f"SELECT * FROM contact WHERE ID='{contactID}'")
+
+        rows = cursorObj.fetchall()
+
+        return rows
+
+    def SearchByName(con,name):
+        cursorObj = con.cursor()
+
+        cursorObj.execute(f"SELECT * FROM contact WHERE Name='{name}'")
+
+        rows = cursorObj.fetchall()
+
+        return rows
+
+    def SearchByFamily(con,family):
+        cursorObj = con.cursor()
+
+        cursorObj.execute(f"SELECT * FROM contact WHERE Family='{family}'")
+
+        rows = cursorObj.fetchall()
+
+        return rows
+
+    def SearchByPhoneNumber(con,phonenumber):
+        cursorObj = con.cursor()
+
+        cursorObj.execute(f"SELECT * FROM contact WHERE PhoneNumber='{phonenumber}'")
+
+        rows = cursorObj.fetchall()
+
+        return rows
