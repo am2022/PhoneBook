6b9ed172ca1fb4a3bce2734eb5e6f924ca12d3f4
am <am2022a2@gmail.com>
2023-02-02 23:41:24 +0330

    delete data.py file

--- venv/src/Backend/data.py
+++
@@ -1,9 +0,0 @@
-import sqlite3
-
-class data:
-    def __init__(self, database_name):
-        self.connection = sqlite3.connect("./" + database_name)
-        self.cursor = connection.cursor()
-
-    def __del__(self):
-        self.connection.close()
