307acf09acdaca167bed9dd9234de6b15969a088
am <am2022a2@gmail.com>
2023-02-03 09:27:56 +0330

    create an object from data class

--- venv/src/Frontend/PhoneBookFront.py
+++ venv/src/Frontend/PhoneBookFront.py
@@ -1,4 +1,3 @@
-#from src.Backend import PhoneBookBack
 import sys
 sys.path.insert(0, '../')
 from Backend import PhoneBookBack
@@ -7,5 +6,7 @@ from tkinter import *
 root_win = Tk()
 root_win.title("phonebook")
 root_win.configure(bg="white")
+
+data = PhoneBookBack.data("PBDB.db")
 
 root_win.mainloop()
